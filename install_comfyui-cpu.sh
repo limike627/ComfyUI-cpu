bcb1cef86bfd14bfba5ac2b137fdc3a2c4afa0f2
Ardenius <ardenius7@gmail.com>
2024-10-04 03:33:12 +0300

    install_comfyui-cpu.sh for linux

--- install_comfyui-cpu.sh
+++ install_comfyui-cpu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip3 install -r requirements.txt
 cd custom_nodes || exit
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git
-python main.py --cpu --cpu-vae
+python3 main.py --cpu --cpu-vae
